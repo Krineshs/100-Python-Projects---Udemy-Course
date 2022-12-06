8f4db2f817459f8ee6869985c24afa1e4d430e2b
Krineshs <shsh_krinesh@yahoo.co.nz>
2022-12-07 12:23:20 +1300

    +Treasure Map +Rock Paper Scissors

--- Project 4 Rock Paper Scissors/treasureMap.py
+++ Project 4 Rock Paper Scissors/treasureMap.py
@@ -4,13 +4,18 @@ row3 = ["O", "O", "O"]
 
 map1 = [row1, row2, row3]
 
-treasure = int(input("Where do you want to put the treasure?"))
+print(f"{row1}\n{row2}\n{row3}")
 
-print(f"{row1}\n{row2}\n{row3}\n")
+treasure = input("Where do you want to put the treasure?")
 
 horizontal = int(treasure[0])
 vertical = int(treasure[1])
 
-print(map1[vertical])
+row = map1[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
 
 
