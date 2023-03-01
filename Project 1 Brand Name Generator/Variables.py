872c7bbaec8d5da369f06e64c2d58c42339e468c
Krineshs <shsh_krinesh@yahoo.co.nz>
2023-03-01 23:01:14 +1300

    t

---
+++ Project 1 Brand Name Generator/Variables.py
@@ -0,0 +1,11 @@
+# A Program to switch around values stored within variables a and b
+
+a = input("Value for A: ")
+b = input("Value for B: ")
+
+c = a
+a = b
+b = c
+
+print("A " + a)
+print("B " + b)
--- Project 1 Brand Name Generator/inputs exercise.py
+++ Project 1 Brand Name Generator/inputs exercise.py
@@ -1,3 +1,9 @@
 # A program to obtain the number of characters in a user's name
 
-userName = input("What is your name?")
+# Executes a print statement that requests a use to input their name and then prints out the length of characters in
+# their name
+
+# print(len(input("What is your name?")))
+
+nameLen = len(input("What is your name?"))
+print(nameLen)
